9c836efa2a4a776a65d921ea47ff80e0f3a3260c
Xadion <105224257+Yadion@users.noreply.github.com>
2024-08-06 16:09:50 +0200

    Update

--- src/js/tablesort.js
+++ src/js/tablesort.js
@@ -1,3 +1,24 @@
+// Suchfunktion
+function searchTable() {
+    // Zugriff auf das Suchfeld und den Suchbegriff
+    const input = document.getElementById('searchInput').value.toLowerCase();
+    const table = document.querySelector("table");
+    const rows = Array.from(table.querySelectorAll("tbody tr"));
+    
+    // Durchlaufen aller Zeilen in der Tabelle
+    rows.forEach(row => {
+        const cells = Array.from(row.getElementsByTagName('td'));
+        const rowVisible = cells.some(cell => cell.textContent.toLowerCase().includes(input));
+        
+        // Zeile anzeigen oder ausblenden basierend auf dem Suchbegriff
+        row.style.display = rowVisible ? '' : 'none';
+    });
+}
+
+// Event-Listener für das Suchfeld hinzufügen
+document.getElementById('searchInput').addEventListener('input', searchTable);
+
+// Sortierfunktion
 function sortTable(columnIndex) {
     var table = document.querySelector("table");
     var rows = Array.from(table.querySelectorAll("tbody tr"));
